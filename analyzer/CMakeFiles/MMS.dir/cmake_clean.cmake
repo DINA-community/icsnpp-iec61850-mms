887b0827cb97cf4ccde7c58cbe202dd127cb6824
Crubumble <39003308+Crubumble@users.noreply.github.com>
2025-07-02 17:21:49 +0200

    hard copy from ot-parser repository

---
+++ analyzer/CMakeFiles/MMS.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "../mms.hlto"
+  "CMakeFiles/MMS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/MMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
